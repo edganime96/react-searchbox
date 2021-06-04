37eb7f4bf576e0e5988f2512bba4fc4ada8e42d8
Edgardo Marcano <edgardojmarcanoc@gmail.com>
2021-06-04 00:15:04 -0500

    Mismo componente creado con class

--- login-udemy/src/App.js
+++ login-udemy/src/App.js
@@ -1,48 +1,95 @@
-import { useReducer, useState } from 'react';
+import React, { useReducer, useState, Component } from 'react';
 
-function App() {
-  // para declarar una const de estado se utiliza el destructuring de un arreglo: 
-  // primero el "Valor" tal cual, y luego el setEr, que es una funcion que actualiza el estado
-  // useState regresa el valor inicial del estado, puede ser indefinido, string vacio "", o cualquier otro valor
+// componente funcional
+// function App() {
+//   // para declarar una const de estado se utiliza el destructuring de un arreglo: 
+//   // primero el "Valor" tal cual, y luego el setEr, que es una funcion que actualiza el estado
+//   // useState regresa el valor inicial del estado, puede ser indefinido, string vacio "", o cualquier otro valor
   
-  const [email, setEmail] = useState(''); 
-  const [password, setPassword] = useState('');
+//   const [email, setEmail] = useState(''); 
+//   const [password, setPassword] = useState('');
 
-  // la primera es una funcion normal, la segunda es una funcion con destructuring. 
-  // Ambas returnan un solo valor (setEmail, setPassword) por lo que pueden ser removidas las llaves {} que las encierran
+//   // la primera es una funcion normal, la segunda es una funcion con destructuring. 
+//   // Ambas returnan un solo valor (setEmail, setPassword) por lo que pueden ser removidas las llaves {} que las encierran
   
-  const handleEmailChange = (event) => setEmail(event.target.value);
-  const handlePasswordChange = ({ target: { value } }) => setPassword(value);
-  
-  console.log(email);
-  console.log(password);
+//   const handleEmailChange = (event) => setEmail(event.target.value);
+//   const handlePasswordChange = ({ target: { value } }) => setPassword(value);
 
-  const handleFormSubmit = (event) => {
-    console.log("Submit");
-    event.preventDefault();
-    // preventDefault previene que el componente actualice la pagina por defecto al hacer click en el boton de tipo "submit"
-    alert(`Usuario: ${email}, Password: ${password}`);
+//   console.log(email);
+//   console.log(password);
+
+//   const handleFormSubmit = (event) => {
+//     console.log("Submit");
+//     event.preventDefault();
+//     // preventDefault previene que el componente actualice la pagina por defecto al hacer click en el boton de tipo "submit"
+//     alert(`Usuario: ${email}, Password: ${password}`);
+//   }
+
+//   return (
+//     <div className="App">
+//       {/* onSubmit es un evento que se va a llamar cuando lo que ocurra dentro de su contenido sea presionado */}
+//       <form onSubmit={handleFormSubmit}>
+//         <h2>Iniciar sesión</h2>
+//         <label>
+//           Correo
+//           <input type="email" value={email} onChange={handleEmailChange} /> 
+//           {/* onChange es el evento de HTML el cual se dispara cuando el usuario esta escribiendo dentro del input */}
+//         </label>
+//         <label>
+//           Contraseña
+//           <input type="password" value={password} onChange={handlePasswordChange} />
+//         </label>
+//         {/* para que el evento onSubmit funcione el botón debe de ser de tipo submit tambien */}
+//         <button type="submit">Entrar</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// componente class
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    }
   }
 
-  return (
-    <div className="App">
-      {/* onSubmit es un evento que se va a llamar cuando lo que ocurra dentro de su contenido sea presionado */}
+  handleEmailChange = (event) => {
+    const { value } = event.target;
+    this.setState({ email: value });
+  }
+
+  handlePasswordChange = (event) => {
+    const { value } = event.target;
+    this.setState({ password: value});
+  }
+
+  render() {
+    const { email } = this.state;
+    const { password } = this.state;
+  
+    const handleFormSubmit = (event) => {
+      event.preventDefault();
+      alert(`Usuario ${email} \n Contraseña ${password}`);
+    }
+    
+    return <div className="App">
       <form onSubmit={handleFormSubmit}>
-        <h2>Iniciar sesión</h2>
+        <h2>Iniciar Sersion</h2>
         <label>
           Correo
-          <input type="email" value={email} onChange={handleEmailChange} /> 
-          {/* onChange es el evento de HTML el cual se dispara cuando el usuario esta escribiendo dentro del input */}
+          <input type='email' value={ email } onChange={ this.handleEmailChange } />
         </label>
         <label>
           Contraseña
-          <input type="password" value={password} onChange={handlePasswordChange} />
+          <input type='password' value= { password } onChange={ this.handlePasswordChange } />
         </label>
-        {/* para que el evento onSubmit funcione el botón debe de ser de tipo submit tambien */}
-        <button type="submit">Entrar</button>
       </form>
     </div>
-  );
+  }
+  
 }
 
 export default App;
